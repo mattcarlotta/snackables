576e954c70a0e34c5c2fe7755792e546e6499678
Matt <carlotta.matt@gmail.com>
2020-10-16 15:37:33 -0700

    Refactor structure

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,18 @@
 
 All notable changes to this project will be documented in this file. See [SemVer 2.0](https://semver.org/) for commit guidelines.
 
+## [0.0.3 - 0.0.4] - 2020-16-08
+
+### Added
+
+- Refactored published structure
+
+## [0.0.2] - 2020-16-08
+
+### Added
+
+- Refactored project structure to expose register to CLI
+
 ## [0.0.1] - 2020-16-08
 
 ### Added
---
+++ register.ts
@@ -0,0 +1,29 @@
+import set from "./src/set";
+import extract from "./src/extract";
+import { logInfo } from "./src/log";
+import { Encoding } from "./types";
+
+const { ENV_LOAD, ENV_DEBUG, ENV_ENCODE } = process.env;
+/**
+ * Loads a single or multiple `.env` file contents into {@link https://nodejs.org/api/process.html#process_process_env | `process.env`}.
+ *
+ */
+((): void => {
+  // check if ENV_LOAD is defined
+  if (ENV_LOAD != null) {
+    const debug = Boolean(ENV_DEBUG);
+
+    // extract and split all .env.* from ENV_LOAD into a parsed object of ENVS
+    const parsedENVs = extract({
+      configs: ENV_LOAD.split(","),
+      debug,
+      encoding: ENV_ENCODE as Encoding
+    });
+
+    // sets ENVS to process.env
+    set(parsedENVs);
+
+    /* istanbul ignore else */
+    if (debug) logInfo(`Assigned ${JSON.stringify(parsedENVs)} to process.env`);
+  }
+})();
