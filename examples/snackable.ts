54fb9f75033d5fbd1e7a0b409de139d1f876bf69
Matt <carlotta.matt@gmail.com>
2020-10-17 19:48:21 -0700

    Add example ansi color

--- examples/snackable.ts
+++ examples/snackable.ts
@@ -1,6 +1,7 @@
+/* eslint-disable no-console */
 import "snackables";
 
-/* eslint-disable-next-line */
-console.log("process.env.MESSAGE", process.env.MESSAGE);
-/* eslint-disable-next-line */
-console.log("process.env.TESTING", process.env.TESTING);
+console.log(`\x1b[32mprocess.env.MESSAGE: ${process.env.MESSAGE}\x1b[0m`);
+console.log(`\x1b[32mprocess.env.TESTING: ${process.env.TESTING}\x1b[0m`);
+
+/* eslint-enable no-console */
