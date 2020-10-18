9d196fcaa2ba1d1c2af65a5f1537c15a9308ebff
Matt <carlotta.matt@gmail.com>
2020-10-17 18:40:33 -0700

    Refactor tsconfig and add snackable example

--- examples/custom.ts
+++ examples/custom.ts
@@ -1,6 +1,6 @@
 import { config } from "snackables";
 
-config({ path: ".env.test" });
+config({ path: ".env.test", debug: true });
 
 /* eslint-disable-next-line */
 console.log("process.env.TESTING", process.env.TESTING);
---
+++ examples/snackable.ts
@@ -0,0 +1,6 @@
+import "snackables";
+
+/* eslint-disable-next-line */
+console.log("process.env.MESSAGE", process.env.MESSAGE);
+/* eslint-disable-next-line */
+console.log("process.env.TESTING", process.env.TESTING);
