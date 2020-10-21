ba778aa63a2c1719c2029b4785b226bf0349e106
Matt <carlotta.matt@gmail.com>
2020-10-20 18:30:43 -0700

    Add more example

---
+++ examples/preloadpath.ts
@@ -0,0 +1,4 @@
+/* eslint-disable-next-line */
+console.log(`\x1b[32mprocess.env.MESSAGE: ${process.env.MESSAGE}\x1b[0m`);
+/* eslint-disable-next-line */
+console.log(`\x1b[32mprocess.env.TESTING: ${process.env.TESTING}\x1b[0m`);
