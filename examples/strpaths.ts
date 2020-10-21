9ccbf86018cde421b90fbff0ab550bfa0576a414
Matt <carlotta.matt@gmail.com>
2020-10-20 18:25:59 -0700

    Add more examples

---
+++ examples/strpaths.ts
@@ -0,0 +1,9 @@
+/* eslint-disable no-console */
+import { config } from "snackables";
+
+config({ path: ".env.base,.env.test", debug: true });
+
+console.log(`\x1b[32mprocess.env.MESSAGE: ${process.env.MESSAGE}\x1b[0m`);
+console.log(`\x1b[32mprocess.env.TESTING: ${process.env.TESTING}\x1b[0m`);
+
+/* eslint-enable no-console */
