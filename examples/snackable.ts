8ece52d7fd21987b8374fcf523e7bf06f74b9e1a
Matt <carlotta.matt@gmail.com>
2020-10-20 21:58:55 -0700

    Add quick run examples script

--- examples/snackable.ts
+++
@@ -1,7 +0,0 @@
-/* eslint-disable no-console */
-import "snackables";
-
-console.log(`\x1b[32mprocess.env.MESSAGE: ${process.env.MESSAGE}\x1b[0m`);
-console.log(`\x1b[32mprocess.env.TESTING: ${process.env.TESTING}\x1b[0m`);
-
-/* eslint-enable no-console */
