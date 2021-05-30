15b948c8b2916dba24f7ac4392165c04f647b064
Matt <carlotta.matt@gmail.com>
2021-05-30 12:17:04 -0700

    Fix types building

--- __tests__/invalidconfig.spec.ts
+++ __tests__/invalidconfig.spec.ts
@@ -10,7 +10,7 @@ describe("Invalid Config", () => {
   it("displays a warning when an invalid config is loaded", () => {
     process.env.LOAD_CONFIG = "development";
 
-    import("../index");
+    require("../index");
 
     expect(logWarning).toHaveBeenCalledWith(
       "Unable to locate a 'development' configuration within 'env.config.json'!"
